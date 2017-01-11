aede1e8460de7696b140dc694a772e8852c514c8
Maggie <Maggie@maggie.local>
2017-01-11 10:34:09 +0800

    Initial commit

---
+++ src/main/java/com/shutup/SharePlatfotmApplication.java
@@ -0,0 +1,12 @@
+package com.shutup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePlatfotmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePlatfotmApplication.class, args);
+	}
+}
